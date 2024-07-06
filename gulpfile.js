a906c0ac76d0418b393ffa851f6a7c0a14c91266
Stephani Medeiros <medeiros.stephani@gmail.com>
2024-07-06 19:41:59 -0300

    tutoria ebac

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/styles/*.scss') /*recupera o arquivo de origem*/
@@ -7,7 +8,14 @@ function styles() {
         .pipe(gulp.dest('./dist/css')); /*destino do css comprimido*/
 }
 
-exports.default = styles;
+function images() {
+    return gulp.src('./src/images/**/*', {encoding: false}) /* ** é pasta, * é arquivo*/
+        .pipe(imagemin()) /*executa a função*/
+        .pipe(gulp.dest('./dist/images')); /*destino do arquivo comprimido*/
+}
+
+exports.default = gulp.parallel(styles, images);
+
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
